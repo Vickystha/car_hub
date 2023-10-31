13af415e11dae5bf507add2312a0de4e9d8cda3a
Bijaya Shrestha <bijayashrestha219@gmail.com>
2023-10-31 16:01:16 +1100

    feat(CustomButton.tsx): Added disabled and type attributes in button

--- app/components/CustomButton.tsx
+++ app/components/CustomButton.tsx
@@ -4,7 +4,7 @@ import { CustomButtonProps } from "@/types"
 
 const CustomButton = ({ containerStyles, title, handleClick } : CustomButtonProps) => {
   return (
-    <button className={`custom-btn ${containerStyles}`} onClick={handleClick}>
+    <button disabled={false} type={"button"} className={`custom-btn ${containerStyles}`} onClick={handleClick}>
         <span className={`flex-1`}>{title}</span>
     </button>
   )
