396cdbfc7a0f336202a0a51c82f81a75d631f118
Bijaya Shrestha <bijayashrestha219@gmail.com>
2023-11-01 19:17:13 +1100

    feat(SearchBar.tsx): Created SearchButton that can be used for SearchBar

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -2,6 +2,24 @@
 import React from 'react'
 import { useState } from 'react'
 import SearchManufacturer from './SearchManufacturer'
+import Image from 'next/image'
+
+
+//SearchButton that immediately returns a button
+const SearchButton = ({otherClasses} : {otherClasses:string}) => (
+
+    <button type='submit' className={`-ml-3 z-10 ${otherClasses}`}>
+        <Image 
+            src={"/magnifying-glass.svg"}
+            alt={"magnifying glass"}
+            width={40}
+            height={40}
+            className='object-contain'
+        />
+
+    </button>
+);
+
 
 
 const SearchBar = () => {
@@ -18,8 +36,9 @@ const SearchBar = () => {
                 setManufacturer = {setManufacturer}
                 
             />
-
+            <SearchButton otherClasses='sm:hidden'/>
         </div>
+        
 
     </form>
   )
