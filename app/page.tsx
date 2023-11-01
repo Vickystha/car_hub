9bdf54ada533a766c452a2488adcce9356497230
Bijaya Shrestha <bijayashrestha219@gmail.com>
2023-11-01 11:28:33 +1100

    feat(page.tsx): Check if cars data is empty, and show a text if not empty else show error message. This is just a starter code for cars card section.

--- app/page.tsx
+++ app/page.tsx
@@ -6,24 +6,41 @@ import { fetchCars } from '@/utils'
 
 export default async function Home() {
   const allCars = await fetchCars();
-  console.log(allCars);
+
+  //Check if all of the cars data is empty
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className='overflow-hidden'>
       <Hero />
       <div className='mt-12 padding-x padding-y max-width' id="discover">
         <div className='home__text-container'>
-            <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
-            <p>Explore out cars you might like</p>
+          <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
+          <p>Explore out cars you might like</p>
         </div>
         <div className="home__filters">
           <SearchBar />
           <div className="home__filter-container">
-            <CustomFilter  title='fuel' />
-            <CustomFilter  title='year' />
-
+            <CustomFilter title='fuel' />
+            <CustomFilter title='year' />
           </div>
-
         </div>
+       
+        {!isDataEmpty ? (
+           //If not empty data show cars card
+          <section>
+            <div className='home__cars-wrapper'>
+              WE HAVE CARS
+            </div>
+              
+          </section>
+        ) : (
+          //If empty data show error message
+          <div className='home__error-container'>
+            <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
+            <p></p>
+          </div>
+        )}
 
 
       </div>
