12b7abf2280bde150b0cf1c1133083ef46c6dee4
Bijaya Shrestha <bijayashrestha219@gmail.com>
2023-11-01 19:25:54 +1100

    feat(SearchBar.tsx): Added search form including car model item and customised serach buttons

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -6,10 +6,10 @@ import Image from 'next/image'
 
 
 //SearchButton that immediately returns a button
-const SearchButton = ({otherClasses} : {otherClasses:string}) => (
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
 
     <button type='submit' className={`-ml-3 z-10 ${otherClasses}`}>
-        <Image 
+        <Image
             src={"/magnifying-glass.svg"}
             alt={"magnifying glass"}
             width={40}
@@ -25,23 +25,44 @@ const SearchButton = ({otherClasses} : {otherClasses:string}) => (
 const SearchBar = () => {
     const handleSearch = () => {
 
-    
+
     }
     const [manufacturer, setManufacturer] = useState("");
-  return (
-    <form className='searchbar' onSubmit={handleSearch}>
-        <div className='searchbar__item'>
-            <SearchManufacturer 
-                manufacturer = {manufacturer}
-                setManufacturer = {setManufacturer}
-                
-            />
-            <SearchButton otherClasses='sm:hidden'/>
-        </div>
-        
+    const [model, setModel] = useState("");
+    
 
-    </form>
-  )
+    return (
+        <form className='searchbar' onSubmit={handleSearch}>
+            <div className='searchbar__item'>
+                <SearchManufacturer
+                    manufacturer={manufacturer}
+                    setManufacturer={setManufacturer}
+
+                />
+                <SearchButton otherClasses='sm:hidden' />
+            </div>
+            <div className='searchbar__item'>
+                <Image
+                    src='/model-icon.png'
+                    width={25}
+                    height={25}
+                    className='absolute w-[20px] h-[20px] ml-4'
+                    alt='car model'
+                />
+                <input
+                    type='text'
+                    name='model'
+                    value={model}
+                    onChange={(e) => setModel(e.target.value)}
+                    placeholder='Tiguan...'
+                    className='searchbar__input'
+                />
+                <SearchButton otherClasses='sm:hidden' />
+            </div>
+            <SearchButton otherClasses='max-sm:hidden' />
+
+        </form>
+    )
 }
 
 export default SearchBar
