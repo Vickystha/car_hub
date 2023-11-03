349c5fdf385609e856cefa9d7c9ab27a9eb1bfae
Bijaya Shrestha <bijayashrestha219@gmail.com>
2023-11-03 16:42:39 +1100

    feat(CustomFilter): Added custom filter feature that filters car data by fuel and year

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import CustomFilter from './components/CustomFilter'
 import { fetchCars } from '@/utils'
 import CarCard from './components/CarCard'
 import { HomeProps } from '@/types'
+import { fuels, yearsOfProduction } from '@/constants'
 
 export default async function Home({searchParams}:HomeProps) {
   const allCars = await fetchCars({
@@ -29,8 +30,8 @@ export default async function Home({searchParams}:HomeProps) {
         <div className="home__filters">
           <SearchBar />
           <div className="home__filter-container">
-            <CustomFilter title='fuel' />
-            <CustomFilter title='year' />
+            <CustomFilter title='fuel' options={fuels}/>
+            <CustomFilter title='year' options={yearsOfProduction}/>
           </div>
         </div>
        
